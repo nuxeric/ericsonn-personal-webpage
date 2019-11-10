eb0f7e8a0ef2456479957e1409bb4bee8fe1e2cf
Eric Son <nuxeric@hotmail.com>
2019-11-10 12:32:54 -0800

    fixed resume header so it wouldnt show up on smaller screens

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+var i = 0;
+var message = 'Hello World!';
+var speed = 50;
+var backspace = false;
+let delay = 25;
+
+function typeWriter() {
+    document.getElementById("hello-world-typing-p").innerHTML = message.substr(0, i);
+    if (i > message.length + delay) {
+        backspace = true;
+    }
+    if (i === 0) {
+        backspace = false;
+    }
+    if (backspace) {
+        i--;
+    } else {
+        i++;
+    }
+    setTimeout('typeWriter()', speed);
+}
+
+
+typeWriter();
+
+
+var cursor = true;
+var speed = 220;
+setInterval(() => {
+    if(cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+    }else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+    }
+}, speed);
